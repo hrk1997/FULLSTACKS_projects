819916b39e020ab175263260008c11d509b2a9c0
hrk <hrk@localhost.localdomain>
2020-10-02 11:18:52 +0530

    backup_ict-fullstacks-projects

---
+++ hrk1997_clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/hrk1997/product_app
+git clone https://github.com/hrk1997/libraryapp_mongodb
+git clone https://github.com/hrk1997/library_casestudy
+git clone https://github.com/hrk1997/library_casestudy
+git clone https://github.com/hrk1997/ass_library
+git clone https://github.com/hrk1997/javascript_exercises
+git clone https://github.com/hrk1997/groceries_hrk
+git clone https://github.com/hrk1997/percentcalc
+git clone https://github.com/hrk1997/calculator
+git clone https://github.com/hrk1997/myproject
+echo "done cloning"
